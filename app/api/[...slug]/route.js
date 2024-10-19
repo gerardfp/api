388fee8b2547136c3e093b75d9487feebc4c11ce
Gerard Falco <gerardfp@users.noreply.github.com>
2024-10-19 10:42:11 +0200

    Update route.js

--- app/api/[...slug]/route.js
+++ app/api/[...slug]/route.js
@@ -1,8 +1,47 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request, { params }) {
- if (params.slug[0] === 'w') {
-   return new Response(`WWW from ${process.env.VERCEL_REGION} ${params}`);
- }
- return new Response(`Normal from ${process.env.VERCEL_REGION} ${params}`);
+
+  let delay = 0;
+  let element = params.slug[0];
+ 
+  if (element === 'w') {
+    delay = 5000;
+    element = params.slug[0]
+  }
+ 
+  let r = '';
+ 
+  switch (element) {
+    case 'integer':
+      r = Math.floor(Math.random() * 2000000);
+      break;
+    case 'float':
+      r = Math.random();
+      break;
+    case 'string':
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+      for(let i = 0; i < 16; i++)
+        r += characters.charAt(Math.floor(Math.random() * characters.length));
+      break;
+    case 'moon';
+      const icons = ["🌕","🌖","🌗","🌘","🌑","🌒","🌓","🌔"];
+      r = icons[Math.floor(Math.random()*icons.length)];
+      break;
+    case 'weather':
+      const icons = ["☀️","🌤","⛅️","🌥","☁️","🌦","🌧","⛈","🌩","🌨"];
+      r = icons[Math.floor(Math.random()*icons.length)];
+      break;
+    case 'wind':
+      r = "💨 " + Math.floor(Math.random() * 200) + " km/h";
+      break;
+    default:
+      r = 'Hello world';
+  }
+
+  if (delay > 0) {
+    await new Promise(r => setTimeout(r, 5000));
+  }
+ 
+  return new Response(r);
 }
