290c5bd87f82a38e06a8bd3f278ec92826b86e0b
Gerard Falco <gerardfp@users.noreply.github.com>
2024-10-19 10:24:34 +0200

    Update route.js

--- app/api/[...slug]/route.js
+++ app/api/[...slug]/route.js
@@ -2,7 +2,7 @@ export const dynamic = 'force-dynamic'; // static by default, unless reading the
  
 export function GET(request, { params }) {
  if (params.slug[0] === 'w') {
-  return new Response(`WWW from ${process.env.VERCEL_REGION} ${params}`);
+   return new Response(`WWW from ${process.env.VERCEL_REGION} ${params}`);
  }
  return new Response(`Normal from ${process.env.VERCEL_REGION} ${params}`);
 }
