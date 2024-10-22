d70c217cae2bcb3bceab629a564c12eb1b2001bd
Gerard Falco <gerardfp@users.noreply.github.com>
2024-10-22 18:20:14 +0200

    .

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request) {
-  return new Response(`Index from ${process.env.VERCEL_REGION}`);
+  return new Response(`https://github.com/gerardfp/api`);
 }
